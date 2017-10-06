ddb48ff8843474af2d16157f891dfe5ce9ea5fa8
Cumhur Erkut <cerkut@users.noreply.github.com>
2017-10-06 19:04:28 +0200

    Update myModulator.m

--- myModulator.m
+++ myModulator.m
@@ -1,17 +1,22 @@
 classdef myModulator < audioPlugin
-       % myModulator audioPlugin by using audioOscillator class
-       
-    properties (Dependent) % These are the properties of audioOscillator, therefore dependent.
-        ff  = 1000    % This will be the fm
+% myModulator audioPlugin by using audioOscillator class
+% Patterns: GUI Toggle controlled by Boolean
+%           Gain control. 
+%           audioOscillator with accessors, deriving SamplesPerFrame from the input frame.
+    
+    properties (Dependent) % These are the properties of audioOscillator we inherit, therefore dependent.
+        ff  = 1000    % Modulator frequency
     end
     
     properties
-        AMP = 1 % This will be the "I" now.
+        iAM = false % Boolean to determine AM (1) or Ring Modulation (0)
+        AMP = 1 % The depth of the Modulation
     end
      properties (Constant)
         PluginInterface = audioPluginInterface( ...
-            audioPluginParameter('AMP','DisplayName','ModIndex I','Mapping',{'lin',0,2}), ...
-            audioPluginParameter('ff','DisplayName','ModFreq f_m','Label','Hz','Mapping',{'log',10,100000}) , ...
+            audioPluginParameter('AMP','DisplayName','Mod Gain','Mapping',{'lin',0,2}), ...
+            audioPluginParameter('ff','DisplayName','Mod Freq','Label','Hz','Mapping',{'log',10,100000}) ,...
+            audioPluginParameter('iAM','DisplayName','Ring (0)/Amp (1) Modulation'), ...
             'InputChannels',1, 'OutputChannels',1)
      end
      
@@ -24,7 +29,7 @@ classdef myModulator < audioPlugin
             obj.pOSC = audioOscillator('sine', 'Frequency', 1000); % Check this class with help audioOscillator
         end
         
-        % We need to implement the accessors only for modulation frequency fm.
+        % We need to implement the accessors only for modulation frequency.
         function set.ff(obj, val)
             obj.pOSC.Frequency = val;
         end
@@ -37,7 +42,7 @@ classdef myModulator < audioPlugin
         
         function out = process (plugin, in)
            plugin.pOSC.SamplesPerFrame = size(in,1); % We are deriving SamplesPerFrame from the input frame!
-           out = (1 + plugin.AMP * plugin.pOSC()).* in ;
+           out = 0.33*(plugin.iAM + plugin.AMP * plugin.pOSC()).* in ; % Scaled for AM (Max AMP=2)
         end
     end
     
